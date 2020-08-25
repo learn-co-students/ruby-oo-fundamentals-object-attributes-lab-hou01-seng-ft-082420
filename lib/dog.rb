88034f2d2715c09a138aa3af87255ecd0baacb03
BenLooper <benlooper08@gmail.com>
2020-08-25 16:54:57 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+    
+    def initialize(name='Fido', breed='Beagle')
+        @name = name
+        @breed = breed
+    end 
+
+    def name=(new_name)
+        @name = name
+    end 
+
+    def name 
+        @name 
+    end 
+
+    def breed=(new_breed)
+        @breed = breed
+    end 
+
+    def breed 
+        @breed 
+    end 
+end 
+
---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person 
+
+    def initialize(name='Bob',job='Coder')
+        @name = name
+        @job = name 
+    end 
+
+    def name=(name)
+        @name = name
+    end 
+
+    def name
+        @name
+    end 
+
+    def job=(job)
+        @job = job 
+    end 
+
+    def job
+        @job
+    end 
+    
+end 
